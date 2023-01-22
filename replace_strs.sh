f6419a62ff265d056d4f8b3f07e49c3721055139
Matt Dawkins <matt.dawkins@kitware.com>
2023-01-22 11:41:41 -0500

    Add new file

---
+++ replace_strs.sh
@@ -0,0 +1,9 @@
+find lib \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/import mmcv/import mmcv_depr as mmcv/g'
+
+find lib \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/from mmcv/from mmcv_depr/g'
+
+find lib \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/import mmdet/import mmdet_depr as mmdet/g'
+
+find lib \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/from mmdet/from mmdet_depr/g'
+
+
