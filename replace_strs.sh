63fa611a20800fb4ce357c5aa030a65b91d06bae
Matt Dawkins <matt.dawkins@kitware.com>
2025-06-15 23:39:48 -0400

    Update replace string

--- replace_strs.sh
+++ replace_strs.sh
@@ -8,7 +8,7 @@ find lib \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s
 
 find lib \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/mmcv_maximum_version = '1.3'/mmcv_maximum_version = '1.3.19'/g"
 
-find lib \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/mmcv./mmcv_depr./g"
+find lib \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/mmcv\./mmcv_depr\./g"
 
 rename 's/mmcv/mmcv_depr/' lib/python3.10/site-packages/*
 rename 's/mmdet/mmdet_depr/' lib/python3.10/site-packages/*
